313abb6a501cf4b8bb97b9d1914d2f8e8f967157
Fred Sauer <fredsa@google.com>
2016-04-20 16:23:17 -0700

    flight controller

---
+++ Assets/_Scripts/FlightController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlightController : MonoBehaviour
+{
+
+	public float speed = .1f;
+
+
+	void Update ()
+	{
+		Vector3 pos = transform.position + transform.forward * speed;
+		pos.x %= 1f;
+//		pos.y %= 1f;
+		pos.z %= 1f;
+		transform.position = pos;
+	}
+}
