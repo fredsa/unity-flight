8ed3a846d25fde3b2350b4466e90712d024a81e0
Fred Sauer <fredsa@google.com>
2016-04-21 09:09:09 -0700

    vr flight

--- Assets/_Scripts/FlightController.cs
+++ Assets/_Scripts/FlightController.cs
@@ -5,14 +5,25 @@ public class FlightController : MonoBehaviour
 {
 
 	public float speed = .1f;
+	public Transform steeringWheel;
+	public GameObject ground;
 
+	Vector2 mainTextureScale;
+
+	void Start() {
+		if (steeringWheel == null) {
+			steeringWheel = Camera.main.transform;
+		}
+		mainTextureScale = ground.GetComponent<MeshRenderer> ().material.mainTextureScale;
+	}
 
 	void Update ()
 	{
-		Vector3 pos = transform.position + transform.forward * speed;
-		pos.x %= 1f;
-//		pos.y %= 1f;
-		pos.z %= 1f;
+		Vector3 direction = steeringWheel.forward;
+		direction.y = 0f;
+		Vector3 pos = transform.position + direction * speed;
+		pos.x %= mainTextureScale.x;
+		pos.z %= mainTextureScale.y;
 		transform.position = pos;
 	}
 }
